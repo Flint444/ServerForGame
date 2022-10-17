22e3b92e9dda441cc6b343833e254ca4dc4f6302
Flint444 <maksimgrabauskas@gmail.com>
2022-10-17 14:45:21 +0200

    2 version #Добавлена проверка Nickname на уникальность

--- auth/users/models.py
+++ auth/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255, unique=True)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     username = None
